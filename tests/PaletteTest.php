6dfc55907939693e9482a9185e60219422b4ed2a
Ismo Vuorinen <ismo.vuorinen@superhelio.com>
2017-09-14 14:20:31 +0300

    Better test name for black and red images

--- tests/PaletteTest.php
+++ tests/PaletteTest.php
@@ -14,7 +14,7 @@ class PaletteTest extends \PHPUnit_Framework_TestCase
         $this->assertInternalType('string', $palette->destination);
     }
 
-    public function test_known_images()
+    public function test_known_images_with_one_color()
     {
         $location = __DIR__ . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR;
         $images = ['black.png' => '000000', 'red.png' => 'CC3333'];
@@ -26,7 +26,7 @@ class PaletteTest extends \PHPUnit_Framework_TestCase
             $palette = new \ivuorinen\Palette\Palette($image);
             $this->assertCount(1, $palette->colorsArray);
             $this->assertArrayHasKey($hex, $palette->colorsArray);
-            $this->assertEquals($image, $palette->filename);            
+            $this->assertEquals($image, $palette->filename);
         }
     }
 }
